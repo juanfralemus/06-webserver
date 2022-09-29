38e29d8d337042dc6b3bba13cf0530b9d2e640bf
Juan Lemus <j.franciscolemus00@gmail.com>
2022-09-29 15:57:36 -0600

    Despliegue app - 2022-09-29

--- app.js
+++ app.js
@@ -17,34 +17,34 @@ app.use( express.static('public') ); // la carpeta pública tiene prioridad sobr
 //  res.send('Home page')
 // })
 
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hola mundo en su respectiva ruta')
+//app.get('/hola-mundo', (req, res) => {
+//    res.send('Hola mundo en su respectiva ruta')
+//});
+
+app.get('*', (req, res) => { // Cualquier otra ruta que no sean las anteriores va a disparar esto
+    res.sendFile( __dirname + '/public/index.html'); // Envío contenido estático. El "__dirname" se refiere al path donde está corriendo la aplicación
 });
 
-// app.get('*', (req, res) => { // Cualquier otra ruta que no sean las anteriores va a disparar esto
-//     res.sendFile( __dirname + '/public/404.html'); // Envío contenido estático. El "__dirname" se refiere al path donde está corriendo la aplicación
-// });
+//app.get('/', (req, res) => { 
+//    res.render('home', {
+//        nombre: 'Fernando Herrera',
+//        titulo: 'Curso de Node'
+//    }); // Cuando se renderiza un archivo de handlebars (hbs), este se busca siempre en la carpeta con nombre "views"
+//});
 
-app.get('/', (req, res) => { 
-    res.render('home', {
-        nombre: 'Fernando Herrera',
-        titulo: 'Curso de Node'
-    }); // Cuando se renderiza un archivo de handlebars (hbs), este se busca siempre en la carpeta con nombre "views"
-});
+//app.get('/generic', (req, res) => { 
+//    res.render('generic', {
+//        nombre: 'Fernando Herrera',
+//        titulo: 'Curso de Node'
+//    });
+//});
 
-app.get('/generic', (req, res) => { 
-    res.render('generic', {
-        nombre: 'Fernando Herrera',
-        titulo: 'Curso de Node'
-    });
-});
-
-app.get('/elements', (req, res) => { 
-    res.render('elements', {
-        nombre: 'Fernando Herrera',
-        titulo: 'Curso de Node'
-    }); 
-});
+//app.get('/elements', (req, res) => { 
+//    res.render('elements', {
+//        nombre: 'Fernando Herrera',
+//        titulo: 'Curso de Node'
+//    }); 
+//});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
